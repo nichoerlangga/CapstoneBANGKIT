b78ba91be9f55c55839483eb7aed22bfb3e4cec1
nichoerlangga <nicholasdavi1201@gmail.com>
2024-06-21 08:25:02 +0700

    Update homescreen and prediction history screen

--- app/src/main/java/com/example/capstonebangkit/screens/HomeScreen.kt
+++ app/src/main/java/com/example/capstonebangkit/screens/HomeScreen.kt
@@ -1,43 +1,67 @@
 package com.example.capstonebangkit.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.capstonebangkit.R
 import com.example.capstonebangkit.components.ButtonComponent
 import com.example.capstonebangkit.components.HeadingTextComponent
 import com.example.capstonebangkit.components.NormalTextComponent
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        color = Color.White,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
     ) {
-        HeadingTextComponent("Welcome to Capstone Bangkit App")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
 
-        NormalTextComponent("Explore our features:")
+            Image(
+                painter = painterResource(id = R.drawable.healhub),
+                contentDescription = "Healhub Logo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(top = 30.dp)
+            )
+            HeadingTextComponent("Welcome to Capstone Bangkit App")
 
-        ButtonComponent("Go to Question") {
-            navController.navigate("QuestionOne")
-        }
-
-        NormalTextComponent("or")
-
-        ButtonComponent("Go to History") {
-            navController.navigate("predictionHistory")
-        }
-
-        NormalTextComponent("or")
-
-        ButtonComponent("Logout") {
-            navController.navigate("logout")
+            Column {
+                NormalTextComponent("Explore our features:")
+                ButtonComponent("Go to Prediction") {
+                    navController.navigate("QuestionOne")
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                ButtonComponent("Go to History") {
+                    navController.navigate("predictionHistory")
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                ButtonComponent("Logout") {
+                    navController.navigate("logout")
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/capstonebangkit/screens/PredictionHistoryScreen.kt
+++ app/src/main/java/com/example/capstonebangkit/screens/PredictionHistoryScreen.kt
@@ -38,23 +38,37 @@
 //}
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.capstonebangkit.R
 import com.example.capstonebangkit.components.ButtonComponent
 import com.example.capstonebangkit.components.HeadingTextComponent
 import com.example.capstonebangkit.components.NormalTextComponent
@@ -64,28 +78,37 @@ import com.example.capstonebangkit.ui.auth.QuestionViewModel
 
 @Composable
 fun PredictionHistoryScreen(navController: NavController, viewModel: QuestionViewModel = viewModel()) {
-    val historyList = viewModel.history.collectAsState().value
-    val sharedPreferencesManager = SharedPreferencesManager(navController.context)
-    val email = sharedPreferencesManager.getEmail() ?: ""
-    val filteredHistory = historyList.filter { it.email == email }
-    Column(
+    Surface(
+        color = Color.White,
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.White)
-            .padding(16.dp)
+            .background(Color.White)
+            .padding(28.dp)
     ) {
-        HeadingTextComponent("Prediction History")
-        LazyColumn(
+        val historyList = viewModel.history.collectAsState().value
+        val sharedPreferencesManager = SharedPreferencesManager(navController.context)
+        val email = sharedPreferencesManager.getEmail() ?: ""
+        val filteredHistory = historyList.filter { it.email == email }
+        Column(
             modifier = Modifier
-                .weight(1f)
-                .padding(vertical = 16.dp)
+                .fillMaxSize()
+                .background(color = Color.White)
+                .padding(16.dp)
         ) {
-            items(filteredHistory) { history ->
-                PredictionHistoryItem(history)
+            HeadingTextComponent("Prediction History")
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(vertical = 16.dp)
+            ) {
+                items(filteredHistory) { history ->
+                    PredictionHistoryItem(history)
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
             }
-        }
-        ButtonComponent("Back to Prediction") {
-            navController.navigate("homescreen")
+            ButtonComponent("Back to Prediction") {
+                navController.navigate("homescreen")
+            }
         }
     }
 }
@@ -94,12 +117,29 @@ fun PredictionHistoryScreen(navController: NavController, viewModel: QuestionVie
 fun PredictionHistoryItem(historyData: HistoryData) {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Color.LightGray)
-            .padding(16.dp)
+            .height(intrinsicSize = IntrinsicSize.Min)
+            .background(colorResource(id = R.color.darkgreen), RoundedCornerShape(8.dp))
+            .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
     ) {
-        NormalTextComponent("Email: ${historyData.email}")
-        NormalTextComponent("Label: ${historyData.label}")
-        NormalTextComponent("Created At: ${historyData.createdAt}")
+
+        NormalTextWhiteComponent("Email: ${historyData.email}")
+        NormalTextWhiteComponent("Label: ${historyData.label}")
+        NormalTextWhiteComponent("Created At: ${historyData.createdAt}")
     }
+}
+
+@Composable
+fun NormalTextWhiteComponent(value:String) {
+    Text(
+        text = value,
+        modifier = Modifier.fillMaxWidth().heightIn(min = 40.dp),
+        style = TextStyle(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Normal
+        ),
+        color = colorResource(id = R.color.white),
+        textAlign = TextAlign.Center
+    )
 }
